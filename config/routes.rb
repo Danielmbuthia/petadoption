fd5d36db7e8c5b1be5913d0dc2ae5fa28368ac8a
Daniel Mbuthia <dambuthia@outlook.com>
2021-07-28 07:24:22 +0300

    profile edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, path: '',controllers: { omniauth_callbacks: 'omniauth_callbacks',registrations: 'registrations' },
-            path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+            path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register',edit: 'profile' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
